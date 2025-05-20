0beba1944a66d9d20e8043e7766ca0f73fa3d888
Caleb Morse <calebmorse.code@gmail.com>
2025-05-19 21:19:04 -0500

    Optimize data flow by passing menuData to MenuPage class, improve separation of concerns amongst classes

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,6 +1,3 @@
-const menuContainer = document.createElement('div')
-menuContainer.classList.add('menu-container')
-
 class MenuList {
 	constructor() {
 		this.menu = []
@@ -14,8 +11,6 @@ class MenuList {
 		return this.menu
 	}
 }
-
-const menu = new MenuList()
 
 class MenuItem {
 	constructor(title, price, desc) {
@@ -32,41 +27,11 @@ class MenuItem {
 	}
 }
 
-const cheeseBurger = new MenuItem(
-	'Cheese Burger',
-	'$15',
-	'A cheese burger with lettuce, tomato, mayo, and pickles. Includes a side of fries and a drink'
-)
-
-const chickenStrips = new MenuItem(
-	'Chicken Strips',
-	'$12',
-	'A basket of 4 chicken strips, served with fries, Texas Toast, and a drink'
-)
-
-const chickenSandwich = new MenuItem(
-	'Chicken Sandwich',
-	'$15',
-	'Our famous chicken sandwich, served with fries, coleslaw, and pickle spear. Optional - toss chicken in buffalo sauce - $2 extra'
-)
-
-menu.addMenuItem(cheeseBurger)
-menu.addMenuItem(chickenStrips)
-menu.addMenuItem(chickenSandwich)
-
-// menu.getMenu().forEach(item => {
-// 	const div = document.createElement('div')
-// 	div.classList.add('menu-item')
-
-// 	div.innerHTML = item.info
-
-// 	menuContainer.appendChild(div)
-// })
-
 class MenuPage {
-	constructor() {
+	constructor(menuData) {
 		this.containerId = 'content'
 		this.title = 'Menu'
+		this.menuData = menuData
 	}
 
 	setContainerId(id) {
@@ -92,12 +57,10 @@ class MenuPage {
 		const menuList = document.createElement('ul')
 		menuList.classList.add('menu')
 
-		menu.getMenu().forEach(item => {
+		this.menuData.forEach(item => {
 			const menuItem = document.createElement('li')
 			menuItem.classList.add('menu-item')
-
 			menuItem.innerHTML = item.info
-
 			menuList.appendChild(menuItem)
 		})
 
@@ -114,6 +77,30 @@ class MenuPage {
 	}
 }
 
-const menuPage = new MenuPage()
+const menu = new MenuList()
+const cheeseBurger = new MenuItem(
+	'Cheese Burger',
+	'$15',
+	'A cheese burger with lettuce, tomato, mayo, and pickles. Includes a side of fries and a drink'
+)
+
+const chickenStrips = new MenuItem(
+	'Chicken Strips',
+	'$12',
+	'A basket of 4 chicken strips, served with fries, Texas Toast, and a drink'
+)
+
+const chickenSandwich = new MenuItem(
+	'Chicken Sandwich',
+	'$15',
+	'Our famous chicken sandwich, served with fries, coleslaw, and pickle spear. Optional - toss chicken in buffalo sauce - $2 extra'
+)
+
+menu.addMenuItem(cheeseBurger)
+menu.addMenuItem(chickenStrips)
+menu.addMenuItem(chickenSandwich)
+const menuData = menu.getMenu()
+
+const menuPage = new MenuPage(menuData)
 
 export { menuPage }
